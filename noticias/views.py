a45355f7e9f1f2cb07956febf7c2c2743c4c74c8
ruben <ruben.baqui.2025117@my.istec.pt>
2026-04-13 19:19:28 +0100

    Commit 4: Implementacao final das views e templates

--- noticias/views.py
+++ noticias/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Artigo, Comentario
 
-# Create your views here.
+def lista_artigos(request):
+    artigos = Artigo.objects.all()
+    return render(request, 'noticias/lista.html', {'artigos': artigos})
+
+def detalhe_artigo(request, pk):
+    artigo = get_object_or_404(Artigo, pk=pk)
+    return render(request, 'noticias/detalhe.html', {'artigo': artigo})
+
+def comentarios_artigo(request, pk):
+    artigo = get_object_or_404(Artigo, pk=pk)
+    if request.method == "POST":
+        texto = request.POST.get('texto')
+        if texto:
+            Comentario.objects.create(artigo=artigo, texto=texto)
+        return redirect('comentarios_artigo', pk=pk)
+    return render(request, 'noticias/comentarios.html', {'artigo': artigo})
