e8573a920f69ae5691db9bd1a66d7bb08f3667b7
ruben <ruben.baqui.2025117@my.istec.pt>
2026-04-13 20:52:11 +0100

    Commit final: Imagem de Luanda e data de publicacao adicionadas

--- noticias/models.py
+++ noticias/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Artigo(models.Model):
     titulo = models.CharField(max_length=200)
     corpo = models.TextField()
+    data_publicacao = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         return self.titulo
